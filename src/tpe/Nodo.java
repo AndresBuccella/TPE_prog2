09dad6dacd142ba3e9e4ae03324de470c2d12d28
Andres Buccella <andresbuccella@gmail.com>
2022-10-28 20:48:56 -0300

    1 2 3 probando

--- src/tpe/Nodo.java
+++ src/tpe/Nodo.java
@@ -1,50 +1,75 @@
 package tpe;
+import java.util.Comparator;
 
-public class Nodo implements Comparable<Nodo>{ //debiera de ser comparator
-	
+import tpe.comparador.*;
+public class Nodo{ //debiera de ser comparator
+
 	private Nodo sigNodo;
 	private int id;
+	private static int contador;
 	
 	public Nodo(int id) {
 		this.id = id;
+		contador = 0;
 	}
 	
-	public void addNodo(Nodo otro) {
+	public void addNodo(Nodo otro, Comparator<Nodo> comp) {
 		
-		if(this.compareTo(otro) == 1) //si es mayor
+		if((comp.compare(this, otro) == 1)) //si es mayor o igual
 			otro.setSigNodo(this);
-		else if(this.compareTo(otro) == 0) //si es igual
+		else if(comp.compare(this, otro) == 0) //si es igual
 			otro.setSigNodo(this);
-		else 
+		else {
 			//this es menor al que se quiere insertar, entonces =>
 			if (this.sigNodo == null) //primero se pregunta si el siguiente esta vacio para no romper
 							//al preguntar si el siguiente es mayor a otro
 				this.setSigNodo(otro);
-			else if (this.sigNodo.compareTo(otro) == 1) {
+			else if (comp.compare(this.sigNodo, otro) == 1) {
 				otro.setSigNodo(this.getSigNodo());
 				this.setSigNodo(otro);
 		}else
-			irAlSiguiente(otro);
+			irAlSiguiente(otro, comp);
+		}
+	}
+	
+	public void irAlSiguiente(Nodo otro, Comparator<Nodo> comp) {
+		this.sigNodo.addNodo(otro, comp);
 	}
 	
 	public void deleteNodo(int pos) {
-		//si pos == 0 entonces lo encontro
+		if(pos == 0) {
+		//si pos == 0 entonces es el primero
+			this.deleteSig();
+		}
 		if((this.sigNodo != null) && (pos > 1)) {
-			
-			deleteNodo(pos--);
+			//si el siguiente no es null y el buscado no es el nodo siguiente, va al siguiente
+			this.sigNodo.deleteNodo(pos--);
 		}else if(this.sigNodo != null) {
 			if((this.sigNodo.getSigNodo() != null) && (pos == 1)) {
-				
+				Nodo sig = this.sigNodo; //aca estamos en 8
+				this.sigNodo = this.sigNodo.getSigNodo(); //7 apunta a 9
+				sig.deleteSig();
 			}
-		}else if (pos == 1) {
-			
 		}
+			
+		
 			
 	}
 	
-	public void irAlSiguiente(Nodo otro) {
-		this.sigNodo.addNodo(otro);
+	public void deleteSig() {
+		this.sigNodo = null;
 	}
+	
+	public int obtenerPos(int id) {
+		if ((this.id != id) && (this.sigNodo != null)) {
+			contador++;
+			this.sigNodo.obtenerPos(id); //no se puede pasar la suma por parámetro
+		}else if ((this.id == id))
+			return contador;
+			
+		return contador;
+	}
+	
 	
 	public void setSigNodo(Nodo sig) {
 		this.sigNodo = sig;
@@ -65,34 +90,34 @@ public class Nodo implements Comparable<Nodo>{ //debiera de ser comparator
 		return this.id + "";
 	}
 	
-	@Override
-	public int compareTo(Nodo o) {
-		if(this.getId() < o.getId())
-			return -1;
-		else if(this.getId() > o.getId())
-			return 1;
-		else
-			return 0;
-	}
 	
 	public static void main(String[] args) {
 		Nodo raiz = new Nodo(1);
+		Comparator comp = new ComparaPorEdad();
 		Nodo n2 = new Nodo(2);
+		Nodo n1 = new Nodo(1);
 		Nodo n3 = new Nodo(3);
 		Nodo n4 = new Nodo(4);
+		Nodo n10 = new Nodo(4);
 		Nodo n5 = new Nodo(5);
 		Nodo n6 = new Nodo(6);
 		Nodo n7 = new Nodo(7);
 		Nodo n8 = new Nodo(8);
 		Nodo n9 = new Nodo(9);
 		//n1.addNodo(n1);
-		raiz.addNodo(n2);
-		raiz.addNodo(n5);
-		raiz.addNodo(n4);
-		raiz.addNodo(n7);
+		n6.addNodo(n7, comp);
+		n6.addNodo(n8, comp);
+		n6.addNodo(n9, comp);
+		n2.addNodo(n3, comp);
+		n2.addNodo(n4, comp);
+		System.out.println(n7.obtenerPos(9));
+		System.out.println(n2.obtenerPos(3));
+		n6.deleteNodo(1);
+		
 
-		System.out.println(raiz);
-		System.out.println(n2); //se puede imprimir el anterior?
+		System.out.println(n6);
+		System.out.println(n7);
+		//System.out.println(n2); //se puede imprimir el anterior? como agregar menor al primero
 	}
 
 
